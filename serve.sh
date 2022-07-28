3e6be12ad66af088a31a50dba63f9e032af0e3dc
MwSpace llc <app@mwspace.com>
2022-07-28 17:21:06 +0200

    updates | fix sendmail to amdmin mail

--- serve.sh
+++ serve.sh
@@ -7,7 +7,7 @@ nextname=${nextvar//[^[:alnum:]]/}
 
 #export NODE_OPTIONS=--openssl-legacy-provider
 rm -R node_modules/
-npm install npm install --save-prod
+npm install --save-prod
 npm run build
 pm2 delete "${nextname,,}"
 pm2 start npm --name "${nextname,,}" -- start -- -p "$1"
