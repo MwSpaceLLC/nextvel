c015b35aa76e632f0bd49c0b384613dbc2fd3a24
MwSpace llc <app@mwspace.com>
2022-07-28 19:38:11 +0200

    updates | fix sendmail to amdmin mail

--- serve.sh
+++ serve.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-# server manager for node / next.js build update app
+# https://dev.to/reactstockholm/setup-a-next-js-project-with-pm2-nginx-and-yarn-on-ubuntu-18-04-22c9
+# server manager for pm2/nextjs
 # Aleksandr Ivanovitch
 
 nextvar="$(pwd)"
 nextname=${nextvar//[^[:alnum:]]/}
 
-#export NODE_OPTIONS=--openssl-legacy-provider
-rm -R node_modules/
-npm install --save-prod
-npm run build
+rm -R node_modules/ # remove prevent mismatch
+rm -R .next/        # remove prevent mismatch
+
+npm install --save-prod # install module prod
+
+npm run build # build nextjs schema
+
+npx prisma migrate deploy #try to migrate updates
+
 pm2 delete "${nextname,,}"
 pm2 start npm --name "${nextname,,}" -- start -- -p "$1"
 pm2 save
