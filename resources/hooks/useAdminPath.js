183825e3c032f4fe79d1a0fb6931d487c19d5230
MwSpace llc <app@mwspace.com>
2022-09-01 11:21:54 +0200

    fix error + success alert

---
+++ resources/hooks/useAdminPath.js
@@ -0,0 +1,17 @@
+import {useEffect, useState} from "react";
+
+export default function useAdminPath() {
+
+    const [location, setLocation] = useState(`/admin`);
+
+    function getReverseToken() {
+        const path = window.location.pathname.split('/');
+        const cursor = path.indexOf('admin', 0) + 1
+
+        setLocation(`/admin/${path[cursor]}`)
+    }
+
+    useEffect(getReverseToken, [])
+
+    return location
+}
